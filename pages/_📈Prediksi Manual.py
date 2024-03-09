7b80ee89c912d9844bdcbc150dcebbb6fd63d5ff
Haris Yunanda Rangkuti <91673258+hayuraaa@users.noreply.github.com>
2024-03-09 22:27:15 +0700

    Update _📈Prediksi Manual.py

--- pages/_📈Prediksi Manual.py
+++ pages/_📈Prediksi Manual.py
@@ -27,7 +27,7 @@ def main():
 
     # Download stock price data
     st.header("Data Download")
-    stock_symbol =st.selectbox("Masukkan Nama Coin:", ["STX4847-USD", "ICP-USD", "RNDR-USD", "AXS-USD", "WEMIX-USD", "SAND-USD", "THETA-USD", "MANA-USD", "APE-USD", "ENJ-USD", "ZIL-USD", "ILV-USD"])
+    stock_symbol =st.selectbox("Masukkan Nama Coin:", ["STX4847-USD", "ICP-USD", "RNDR-USD", "AXS-USD", "WEMIX-USD", "SAND-USD", "THETA-USD", "MANA-USD", "APE-USD", "ENJ-USD", "ZIL-USD", "ILV-USD", "EGLD-USD", "MASK8536-USD", "SUSHI-USD"])
     start_date = st.date_input("Start Date", pd.to_datetime("2022-01-01"))
     end_date = st.date_input("End Date", pd.to_datetime("2023-01-01"))
 
