d9d9dfe93bad60437fce5696bc8abe2e707b7aea
Haris Yunanda Rangkuti <91673258+hayuraaa@users.noreply.github.com>
2024-07-24 15:14:07 +0000

    Update

--- 1_Home.py
+++ 1_Home.py
@@ -11,7 +11,7 @@ def main():
     # Dataframe selection
     st.markdown("<h1 align='center'> <b> Sistem Prediksi Cryptocurrency Ekosistem Metaverse", unsafe_allow_html=True)
     new_line(1)
-    st.markdown("Selamat datang! Sebuah aplikasi prediksi yang intuitif dan kuat yang dirancang untuk menyederhanakan proses membangun dan mengevaluasi model pembelajaran mesin. Sistem Prediksi ini menggunakan Algoritma Long Short Term Memory dan Gated Recurrent Unit, sistem ini diharapkan dapat menjadi landasan untuk strategi perdagangan yang lebih cerdas dan keputusan investasi yang lebih baik dalam cryptocurrency.", unsafe_allow_html=True)
+    st.markdown("Selamat datang!!! Sebuah aplikasi prediksi yang intuitif dan kuat yang dirancang untuk menyederhanakan proses membangun dan mengevaluasi model pembelajaran mesin. Sistem Prediksi ini menggunakan Algoritma Long Short Term Memory dan Gated Recurrent Unit, sistem ini diharapkan dapat menjadi landasan untuk strategi perdagangan yang lebih cerdas dan keputusan investasi yang lebih baik dalam cryptocurrency.", unsafe_allow_html=True)
     
     st.divider()
     
